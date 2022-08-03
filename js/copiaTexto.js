51e7ec7aaa4ff724fa2f10646de43b545d4acbeb
Raphito <raphaelpereirabarbosa2311@gmail.com>
2022-08-03 16:52:58 -0300

    adicionar servidor do paulo

--- js/copiaTexto.js
+++ js/copiaTexto.js
@@ -2,7 +2,7 @@ let trocaText = document.querySelector('#trocaText')
 let containerDireito = document.querySelector('#containerDireito')
 
 containerDireito.addEventListener('click', function(){
-    navigator.clipboard.writeText('127.0.0.1')
+    navigator.clipboard.writeText('pixelmonxyz.enxada.host')
     trocaText.innerHTML = 'Ip copiado com sucesso!'
     
 });
