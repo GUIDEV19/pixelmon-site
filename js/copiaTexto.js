f8b10c9b83d37f94abd2789cb8feb8c4a3b10302
Guilherme <guilherme@zscansoftware.com>
2022-08-03 21:57:50 -0300

    Removendo espaços no arquivo copiaTexto

--- js/copiaTexto.js
+++ js/copiaTexto.js
@@ -4,6 +4,5 @@ let containerDireito = document.querySelector('#containerDireito')
 containerDireito.addEventListener('click', function(){
     navigator.clipboard.writeText('pixelmonxyz.enxada.host')
     trocaText.innerHTML = 'Ip copiado com sucesso!'
-    
 });
 
