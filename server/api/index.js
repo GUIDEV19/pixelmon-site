9812eb7528695abc11f6d2c737209e0c0abb49a2
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-08-23 22:07:23 -0300

    Criação de rotas para teste de api
    Adicionado as rotas post, put e delete, editado também a rota anterior get, para que o bem vindo a api ficasse no arquivo inicial da api, e o teste fosse feito também no metodo get, testado no insonia e todas as rotas funcionaram corretamente.

--- server/api/index.js
+++ server/api/index.js
@@ -7,10 +7,52 @@ app.use(bodyParser.json())
 
 const port = 3000
 
-app.get('/teste', (req, res) =>{
+
+app.get('/', (req, res) =>{
+    //page inicia da api
     res.status(200).send({mensagem: 'boas-vindas à API'})
+    
 });
+
+
+app.get('/teste', (req, res) =>{
+    //teste dp metodo get
+    return res.json({
+        status: "Get está funcionando"
+    })
+
+    
+});
+
+
+app.post('/teste', (req, res)=>{
+    //teste do metodo post
+    return res.json({
+        status: "Post está funcionando"
+    })
+
+})
+
+app.put('/teste', (req, res) => {
+    //teste do metodo put
+    return res.json({
+        status: "Put está funcionando"
+    })
+
+})
+
+app.delete('/teste', (req, res) => {
+
+    //teste do metodo delete
+    return res.json({
+        status: "Delete está funcionando"
+    })
+
+
+})
+   
 
 app.listen(port, () => console.log('servidor rodando na porta ' + port))
 
 module.exports = app
+
