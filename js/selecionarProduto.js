463a659df9b4cb1bda1c30bcd8e0a8d24feb00ef
Raphito <raphaelpereirabarbosa2311@gmail.com>
2022-08-23 21:39:31 -0300

    adicao de animacao ao comprar, transition, cor e +

--- js/selecionarProduto.js
+++ js/selecionarProduto.js
@@ -11,7 +11,7 @@ vip30dias.addEventListener('click', function(){
     //Mudar texto, valor e produto selecionado
     textoProdutoSelecionado.innerHTML = 'VIP 30 DIAS'
     vip30dias.style.backgroundColor = 'lightblue'
-    vip30dias.style.border = '2px solid blue'
+    vip30dias.style.border = '3px solid yellow'
 
     descricaoProduto.textContent = 'Comprando o VIP de 30 dias você garante 1 mês de muitos benefícios! Usufrua de todas as vantagens VIP e ajude o servidor a continuar gratuito e com constantes atualizações.'
 
@@ -20,15 +20,15 @@ vip30dias.addEventListener('click', function(){
     //Tirar as formatações dos outros (tirar a borda e voltar a cor de fundo ao normal)
     vip60dias.style.border = 'none'
     vip90dias.style.border = 'none'
-    vip60dias.style.backgroundColor = 'rgb(121, 168, 255)'
-    vip90dias.style.backgroundColor = 'rgb(121, 168, 255)'
+    vip60dias.style.backgroundColor = 'rgb(82, 143, 255)'
+    vip90dias.style.backgroundColor = 'rgb(82, 143, 255)'
 })
 
 vip60dias.addEventListener('click', function(){
     //Mudar texto, valor e produto selecionado
     textoProdutoSelecionado.innerHTML = 'VIP 60 DIAS'
     vip60dias.style.backgroundColor = 'lightblue'
-    vip60dias.style.border = '2px solid blue'
+    vip60dias.style.border = '3px solid yellow'
 
     descricaoProduto.textContent = 'Comprando o VIP de 60 dias você garante 2 meses de muitos benefícios! Usufrua de todas as vantagens VIP e ajude o servidor a continuar gratuito e com constantes atualizações.'
 
@@ -37,8 +37,8 @@ vip60dias.addEventListener('click', function(){
     //Tirar as formatações dos outros (tirar a borda e voltar a cor de fundo ao normal)
     vip30dias.style.border = 'none'
     vip90dias.style.border = 'none'
-    vip30dias.style.backgroundColor = 'rgb(121, 168, 255)'
-    vip90dias.style.backgroundColor = 'rgb(121, 168, 255)'
+    vip30dias.style.backgroundColor = 'rgb(82, 143, 255)'
+    vip90dias.style.backgroundColor = 'rgb(82, 143, 255)'
 
 })
 
@@ -46,7 +46,7 @@ vip90dias.addEventListener('click', function(){
     //Mudar texto, valor e produto selecionado
     textoProdutoSelecionado.innerHTML = 'VIP 90 DIAS'
     vip90dias.style.backgroundColor = 'lightblue'
-    vip90dias.style.border = '2px solid blue'
+    vip90dias.style.border = '3px solid yellow'
 
     descricaoProduto.textContent = 'Comprando o VIP de 90 dias você garante 3 meses de muitos benefícios! Usufrua de todas as vantagens VIP e ajude o servidor a continuar gratuito e com constantes atualizações.'
 
@@ -55,8 +55,8 @@ vip90dias.addEventListener('click', function(){
     //Tirar as formatações dos outros (tirar a borda e voltar a cor de fundo ao normal)
     vip60dias.style.border = 'none'
     vip30dias.style.border = 'none'
-    vip60dias.style.backgroundColor = 'rgb(121, 168, 255)'
-    vip30dias.style.backgroundColor = 'rgb(121, 168, 255)'
+    vip60dias.style.backgroundColor = 'rgb(82, 143, 255)'
+    vip30dias.style.backgroundColor = 'rgb(82, 143, 255)'
 })
 
 ;
