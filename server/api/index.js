27496f3436535d4c6018a3c6cbd6112b989c24e1
Guilherme <guilherme@zscansoftware.com>
2022-08-23 23:11:40 -0300

    Configuração inicial API mercadoPago

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mercadopago = require('mercadopago')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 const port = 3000
+
+mercadopago.configure({
+	access_token: "TEST-7001311731677237-082321-0f27bff2dba64b5b3b3e69043e3c96ce-241447996",
+});
+
+let preference = {
+        items: [
+        {
+            title: 'VIP 30 Dias',
+            unit_price: 30,
+            quantity: 1,
+        },
+        {
+            title: 'VIP 60 Dias',
+            unit_price: 60,
+            quantity: 1,
+        },
+        {
+            title: 'VIP 90 Dias',
+            unit_price: 90,
+            quantity: 1,
+        }
+        ]
+    };
+    
+    mercadopago.preferences.create(preference)
+    .then(function(response){
+    // Este valor substituirá a string "<%= global.id %>" no seu HTML
+        global.id = response.body.id;
+    }).catch(function(error){
+        console.log(error);
+    });
+
+
 
 
 app.get('/', (req, res) =>{
