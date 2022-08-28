01aee373d93a64ff8c66178a147b7f6120301c4e
Guilherme <guilherme@zscansoftware.com>
2022-08-28 12:40:32 -0300

    Adicionando configuração de tela mercado pago

--- js/mercadoPago.js
+++ js/mercadoPago.js
@@ -1,3 +1,12 @@
+let vipx30dias = document.getElementById('vipx30dias')
+let vipx60dias = document.getElementById('vipx60dias')
+let vipx90dias = document.getElementById('vipx90dias')
+
+vipx30dias.addEventListener('click', () =>{
+    
+});
+
+
 const mp = new MercadoPago('TEST-175c9ea0-6a24-43f5-b35a-4d9a5c1f7180', {
     locale: 'pt-BR'
 });
