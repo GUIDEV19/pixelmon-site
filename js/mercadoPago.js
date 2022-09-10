3d8bb4124022f50ef107dbfe53301e9848a5a67a
Guilherme <guilherme@zscansoftware.com>
2022-09-09 22:11:55 -0300

    Alteração de chave MP

--- js/mercadoPago.js
+++ js/mercadoPago.js
@@ -1,40 +1,33 @@
 var teste = document.querySelectorAll(".produto")
 var testinho = document.querySelectorAll('.invisivel')
+var button = document.querySelector('.button')
 
 for(i = 0 ;   i < teste.length ; i++){
-
     let umaVar = testinho[i].textContent
-
     teste[i].addEventListener('click', function(){
+        button.addEventListener('click', () =>{
         fetch(`http://localhost:3000/geraChave/?vip=${umaVar}`).then(function(response) {
-
             return response.json();
-
-          }).then(function(data) {
-
+        }).then(function(data) {
             const chave = data.chave
             console.log(chave)
             const mp = new MercadoPago('TEST-175c9ea0-6a24-43f5-b35a-4d9a5c1f7180', {
                 locale: 'pt-BR'
-            });
-            
-            mp.checkout({
+            })
+            const checkout = mp.checkout({
                 preference: {
-                    id: chave
-                },
-                render: {
-                    container: '.button',
-                    label: 'Comprar',
+                    id: chave 
                 }
-            });
-
-          }).catch(function() {
-
+                
+            })
+            
+            checkout.open()  
+            
+            }).catch(function() {
             console.log("Houve algum problema!");
-
-          });
+        });
+        });
         
     })
-
 }
     
